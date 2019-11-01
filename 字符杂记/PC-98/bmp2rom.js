a756a0d7309b21a989c11bc3d2a56490eee69a0e
mrhso <liu.songquan@live.cn>
2019-11-01 15:43:53 +0800

    Update bmp2rom.js

--- 字符杂记/PC-98/bmp2rom.js
+++ 字符杂记/PC-98/bmp2rom.js
@@ -4,11 +4,15 @@
 const path = require('path');
 const fs = require('fs');
 
-let inputFile = process.argv.slice(2)[0];
+let args = process.argv.slice(2);
+let usage = 'node bmp2rom.js <input font.bmp> <output font.rom>';
+
+let inputFile = args[0];
 if (!inputFile) {
-    throw 'node bmp2rom.js <input font.bmp> <output font.rom>';
+    console.error(usage);
+    process.exit(1);
 };
-let outputFile = process.argv.slice(2)[1] || `${inputFile.substring(0, inputFile.length - path.extname(inputFile).length)}.rom`;
+let outputFile = args[1] || `${inputFile.substring(0, inputFile.length - path.extname(inputFile).length)}.rom`;
 
 let bmp = fs.readFileSync(inputFile);
 
